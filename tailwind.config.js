cb509fc1afe4fcd14b43d4441ccd1db88e43451e
Jeevan Aaron <jeevan.aaron.joseph@gmail.com>
2022-10-14 23:55:18 +0530

    brand colors made as variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+          brand: {
+            black: "#000F1C",
+            dark: "#0B1828",
+            DEFAULT: "#50A6D6",
+            grey: "#475466",
+            white: "#FFFFFF",
+        },
+      },
+    },
   },
   plugins: [],
 };
